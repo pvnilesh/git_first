6818ec24c2ecacc42d07470818f64574e8b9ea6d
nilesh <nilesh@git.com>
2017-07-02 10:14:05 +0530

    commit 3 for Hello.java, extend loop

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello
   public static void main(String[] args)
   {
     System.out.println("Hello There!");
-    for(int i=0; i<5; i++)
+    for(int i=0; i<10; i++)
     {
       System.out.println("loop- " + i); 
     }
